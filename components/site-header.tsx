70a0210542d8a55db67d8289ed33ceae1af9339b
Kyle Ledbetter <kyle@kyleledbetter.com>
2024-11-17 11:00:31 -0600

    feat(header): remove dream links
    Signed-off-by: Kyle Ledbetter <kyle@kyleledbetter.com>

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -13,37 +13,7 @@ export function SiteHeader() {
       <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
         <MainNav items={siteConfig.mainNav} />
         <div className="flex flex-1 items-center justify-end space-x-4">
-          <nav className="flex items-center space-x-1">
-            <Link
-              href={siteConfig.links.linkedin}
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div
-                className={buttonVariants({
-                  size: "icon",
-                  variant: "ghost",
-                })}
-              >
-                <Linkedin className="size-5  fill-current" />
-                <span className="sr-only">LinkedIn</span>
-              </div>
-            </Link>
-            <Link
-              href={siteConfig.links.twitter}
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div
-                className={buttonVariants({
-                  size: "icon",
-                  variant: "ghost",
-                })}
-              >
-                <Icons.twitterX className="size-5 fill-current" />
-                <span className="sr-only">Twitter</span>
-              </div>
-            </Link>
+          <nav className="flex items-end space-x-1">
             <ThemeToggle />
           </nav>
         </div>
